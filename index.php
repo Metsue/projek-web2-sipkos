3bd4f7903db2db30b717e3667110256509908d27
RidhoRIdhoRidhoRIdoh <ridhojuli78@gmail.com>
2026-05-11 21:03:00 +0800

    dkeekekekkk

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <?php
         echo "Hello, wordl, anuu";
         echo "<br>";
-        echo "Hello, wordl, anuubrsdfcvgjhbhv";
+        echo "Hello, wordl, anudmklkl";
     ?>
     
 </body>
